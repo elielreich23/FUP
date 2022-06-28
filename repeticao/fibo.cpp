b87d4d6e3ab87796e0d1ce003f80baa747cf0b7a
ELIEL DOUNA <elielreich@yahoo.com>
2022-06-28 14:15:14 -0300

    1
    updates

--- repeticao/fibo.cpp
+++ repeticao/fibo.cpp
@@ -1,45 +1,19 @@
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int x, y, som = 0;
-//     cin >> x;
-//     y = x;
-//     int vetor[y];
-//     for (int i = 0; i < y; i++)
-//     {
-
-//         som++;
-
-//         cout << som << " ";
-
-//          if (i == 2; i < y; (vetor[i] > vetor[i - 1]))
-//          {
-//            b = vetor[i] + vetor[i - 1];
-//              cout << som << " ";
-//          }
-//     }
-// }
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n1 = 0, n2 = 1, n3, i, x, num_par, b = 0;
+    int n1 = 0, n2 = 1, n3, i, number, soma = 0;
     cout << "Enter the number of elements: ";
-    cin >> x;
-    for (i = 0; i < x; i++)
+    cin >> number;
+    for (i = 0; i < number; ++i)
     {
         n3 = n1 + n2;
+        if (n3 % 2 == 0)
+            soma += n3;
         // cout << n3 << " ";
         n1 = n2;
         n2 = n3;
-        if (n3 % 2 == 0)
-        {
-            num_par = n3;
-            b = num_par + num_par;
-            cout << b;
-        }
     }
-
+    cout << soma << endl;
     return 0;
 }
--- repeticao/jogo_concentracao.cpp
+++ repeticao/jogo_concentracao.cpp
@@ -14,27 +14,4 @@ int main()
         aux++;
     }
     cout << " ]" << endl;
-
-    // int A, B, x, y, c;
-    // cin >> A >> B;
-    // for (int i = 0; i < B; i++)
-    // {
-    //     x = A++;
-    //     c = B;
-    //     if (int j = c)
-    //     {
-    //         y = c--;
-    //     }
-    //     cout << x << y;
-    // }
-
-    // for (int j = B; j >= A; j--)
-    // {
-    //     y = j--;
-    //     cout << y << "\n";
-    // }
-
-    // cout << x << "\n";
-    // 1 5
-    // 1 5 2 4 3 3 4 2 5 1
 }
